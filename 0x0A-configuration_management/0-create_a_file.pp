22bac4cffa7131c064750369af846727898c6f12
mohammed <hj72475@gmail.com>
2024-06-29 03:34:56 +0100

    edit puppet errors

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 #creating a file in /tmp using puppet
 
 file {'test':
- path   => '/tmp/school',
- mode   => '0744',
- owner  => 'www-data',
- group  => 'www-data',
- content  => 'I love Puppet',
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
